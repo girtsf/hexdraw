fb021e0f9fbedabd7f8cebd63960b748e43e5a0c
Girts <girts.folkmanis@gmail.com>
2015-12-31 14:26:14 -0800

    Switch to Konva.

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const TRIANGLE_SIZE = 50;  // pixels
 const TRIANGLE_INSET = 3;  // pixels
 
 class HexGrid {
-  constructor(canvasName) {
-    const canvas = document.getElementById(canvasName);
-    this._ctx = canvas.getContext('2d');
+  constructor(container) {
+    this._stage = new Konva.Stage({
+      container: container,
+      width: 500,
+      height: 500,
+    });
+    this._layer = new Konva.Layer();
+    this._stage.add(this._layer);
+    //  const canvas = document.getElementById(canvasName);
+    // this._ctx = canvas.getContext('2d');
   }
 
   getRandomInt(min, max) {
@@ -26,50 +33,52 @@ class HexGrid {
     return TRIANGLE_SIZE * Math.cos(Math.PI / 3);
   }
 
-  drawLeftTriangle(ctx, r, g, b, a) {
-    ctx.strokeStyle = 'rgb(0, 0, 0)';
+  drawTriangle(group, points, r, g, b, a) {
     const fill = `rgba(${r}, ${g}, ${b}, ${a})`;
-    ctx.fillStyle = fill;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(this.xOffset(), this.yOffset());
-    ctx.lineTo(0, TRIANGLE_SIZE);
-    ctx.lineTo(0, 0);
-    ctx.fill();
-    ctx.stroke();
+    let poly = new Konva.Line({
+      points: points,
+      fill: fill,
+      stroke: 'black',
+      strokeWidth: 1,
+      closed: true,
+    });
+    group.add(poly);
   }
 
-  drawRightTriangle(ctx, r, g, b, a) {
-    const fill = `rgba(${r}, ${g}, ${b}, ${a})`;
-    ctx.fillStyle = fill;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, TRIANGLE_SIZE);
-    ctx.lineTo(-this.xOffset(), this.yOffset());
-    ctx.lineTo(0, 0);
-    ctx.fill();
-    ctx.stroke();
+  drawLeftTriangle(group, x, y, r, g, b, a) {
+    const points = [x, y, x + this.xOffset(), y + this.yOffset(), x, y + TRIANGLE_SIZE];
+    this.drawTriangle(group, points, r, g, b, a);
+  }
+
+  drawRightTriangle(group, x, y, r, g, b, a) {
+    const points = [x, y, x, y + TRIANGLE_SIZE, x - this.xOffset(), y + this.yOffset()];
+    this.drawTriangle(group, points, r, g, b, a);
   }
 
   drawSlice(wedges) {
-    let ctx = this._ctx;
+    let group = new Konva.Group({
+      x: 100,
+      y: 50,
+    });
     for (let col = 0; col < wedges; col++) {
       const rows = wedges - col;
       for (let row = 0; row < rows; row++) {
-        ctx.save();
-        ctx.translate(col * this.xOffset(), row * TRIANGLE_SIZE + col * this.yOffset());
-        this.drawLeftTriangle(ctx, this.randomColor(), this.randomColor(), this.randomColor(), 0.7);
+        let x = col * this.xOffset();
+        let y = row * TRIANGLE_SIZE + col * this.yOffset();
+        this.drawLeftTriangle(group, x, y, this.randomColor(), this.randomColor(), this.randomColor(), 0.7);
         if (row !== (rows - 1)) {
-          ctx.translate(this.xOffset(), this.yOffset());
-          this.drawRightTriangle(ctx, this.randomColor(), this.randomColor(), this.randomColor(), 0.7);
+          x += this.xOffset();
+          y += this.yOffset();
+          this.drawRightTriangle(group, x, y, this.randomColor(), this.randomColor(), this.randomColor(), 0.7);
         }
-        ctx.restore();
       }
     }
+    this._layer.add(group);
+    this._layer.draw();
   }
 }
 
 function draw() {
-  let hex = new HexGrid('canvas')
+  let hex = new HexGrid('container')
   hex.drawSlice(3);
 }

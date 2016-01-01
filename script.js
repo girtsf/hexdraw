ec94bb92dbcbcdc4567713adf2316e2f3168d75f
Girts <girts.folkmanis@gmail.com>
2015-12-31 21:21:20 -0800

    Blinky blinks. Kaleidoscope works.

--- script.js
+++ script.js
@@ -3,8 +3,33 @@
 const TRIANGLE_SIZE = 20;  // pixels
 const TRIANGLE_INSET = 3;  // pixels
 
+// Returns a random int [min, max).
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function makeTriangularArray(size, constructorFn) {
+  let data = [];
+  for (let col = 0; col < size; col++) {
+    let colData = [];
+    for (let row = 0; row < (size - col) * 2 - 1; row++) {
+      colData.push(constructorFn());
+    }
+    data.push(colData);
+  }
+  return data;
+}
+
+function getRandomFill() {
+  const r = getRandomInt(0, 256);
+  const g = getRandomInt(0, 256);
+  const b = getRandomInt(0, 256);
+  return `rgba(${r}, ${g}, ${b}, 0.8)`;
+}
+
+// Handles drawing a hex grid.
 class HexGrid {
-  constructor(container) {
+  constructor(size, container) {
     this._stage = new Konva.Stage({
       container: container,
       width: 500,
@@ -12,15 +37,8 @@ class HexGrid {
     });
     this._layer = new Konva.Layer();
     this._stage.add(this._layer);
-  }
-
-  getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  randomColor() {
-    const c = this.getRandomInt(0, 256);
-    return c;
+    this._size = size;
+    this._triangles = makeTriangularArray(size, () => []);
   }
 
   xOffset() {
@@ -31,8 +49,13 @@ class HexGrid {
     return TRIANGLE_SIZE * Math.cos(Math.PI / 3);
   }
 
-  drawTriangle(group, points, r, g, b, a) {
-    const fill = `rgba(${r}, ${g}, ${b}, ${a})`;
+  drawTriangle(group, points, col, row) {
+    let fill;
+    if (this._triangles[col][row].length) {
+      fill = this._triangles[col][row][0].fill();
+    } else {
+      fill = getRandomFill();
+    }
     let poly = new Konva.Line({
       points: points,
       fill: fill,
@@ -40,46 +63,72 @@ class HexGrid {
       strokeWidth: 1,
       closed: true,
     });
+    this._triangles[col][row].push(poly);
     group.add(poly);
   }
 
-  drawLeftTriangle(group, x, y, r, g, b, a) {
+  drawLeftTriangle(group, x, y, col, row) {
     const points = [x, y, x + this.xOffset(), y + this.yOffset(), x, y + TRIANGLE_SIZE];
-    this.drawTriangle(group, points, r, g, b, a);
+    this.drawTriangle(group, points, col, row);
   }
 
-  drawRightTriangle(group, x, y, r, g, b, a) {
+  drawRightTriangle(group, x, y, col, row) {
     const points = [x, y, x, y + TRIANGLE_SIZE, x - this.xOffset(), y + this.yOffset()];
-    this.drawTriangle(group, points, r, g, b, a);
+    this.drawTriangle(group, points, col, row);
   }
 
-  drawSlice(wedges, rotation) {
+  drawSlice(rotation, flip) {
     let group = new Konva.Group({
       x: 250,
       y: 250,
       rotation: rotation,
+      scaleX: flip ? -1 : 1,
+      //scaleY: flip ? -1 : 1,
     });
-    for (let col = 0; col < wedges; col++) {
-      const rows = wedges - col;
+    for (let col = 0; col < this._size; col++) {
+      const rows = this._size - col;
       for (let row = 0; row < rows; row++) {
         let x = col * this.xOffset();
         let y = row * TRIANGLE_SIZE + col * this.yOffset();
-        this.drawLeftTriangle(group, x, y, this.randomColor(), this.randomColor(), this.randomColor(), 0.7);
+        this.drawLeftTriangle(group, x, y, col, row * 2);
         if (row !== (rows - 1)) {
           x += this.xOffset();
           y += this.yOffset();
-          this.drawRightTriangle(group, x, y, this.randomColor(), this.randomColor(), this.randomColor(), 0.7);
+          this.drawRightTriangle(group, x, y, col, row * 2 + 1);
         }
       }
     }
     this._layer.add(group);
     this._layer.draw();
   }
+
+  draw() {
+    for (let i = 0; i < 6; i++) {
+      this.drawSlice(0 + 60 * (i - i % 2), i % 2);
+    }
+  }
+
+  changeRandomTriangle() {
+    const col = getRandomInt(0, this._size);
+    const row = getRandomInt(0, (this._size - col) * 2 - 1);
+
+    const fill = getRandomFill();
+
+    const triangles = this._triangles[col][row];
+    triangles.forEach(t => {
+      t.setFill(fill);
+      t.draw();
+    });
+  }
+
 }
 
 function draw() {
-  let hex = new HexGrid('container')
-  for (let i = 0; i < 6; i++) {
-    hex.drawSlice(10, 0 + 60 * i);
-  }
+  const size = 10;
+  let hex = new HexGrid(size, 'container');
+  hex.draw();
+  setInterval(() => {
+    hex.changeRandomTriangle();
+    //hex._stage.batchDraw();
+  }, 10);
 }
